d7eb3a5d8defb84f849e70ffc7e4f48d96e44e72
Michael Bargatin <attractadore02@gmail.com>
2020-11-20 19:05:46 +0300

    Update disassembler

--- src/disassemble.c
+++ src/disassemble.c
@@ -1,17 +1,19 @@
 #include "commands.h"
+#include "label.h"
 #include "util.h"
 
 #include <assert.h>
-#include <ctype.h>
-#include <stdbool.h>
-#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
+#define COMMAND_INDENT "    "
+
 typedef enum disassembly_error_e {
     DISASSEMBLY_OK,
+    DISASSEMBLY_OOM,
     DISASSEMBLY_INVALID_COMMAND,
-    DISASSEMBLY_INVALID_ARGUMENT_COUNT,
+    DISASSEMBLY_INVALID_REGISTER_ID,
+    DISASSEMBLY_INVALID_JUMP_ADDRESS,
     DISASSEMBLY_CORRUPTION,
 } DisassemblyError;
 
@@ -19,10 +21,14 @@ char const* getDisassemblyErrorString(DisassemblyError e) {
     switch (e) {
         case DISASSEMBLY_OK:
             return "No disassembly error";
+        case DISASSEMBLY_OOM:
+            return "Memory exhausted during disassembly";
         case DISASSEMBLY_INVALID_COMMAND:
             return "Binary contains invalid command code";
-        case DISASSEMBLY_INVALID_ARGUMENT_COUNT:
-            return "Binary contains command with invalid argument count";
+        case DISASSEMBLY_INVALID_REGISTER_ID:
+            return "Binary contains command with invalid register id";
+        case DISASSEMBLY_INVALID_JUMP_ADDRESS:
+            return "Binary contains jump command to invalid address";
         case DISASSEMBLY_CORRUPTION:
             return "Binary is corrupted";
         default:
@@ -31,38 +37,277 @@ char const* getDisassemblyErrorString(DisassemblyError e) {
     assert(!"No error returned string returned");
 }
 
+#define ADVANCE_CHECK(end, ip, step, l) \
+    do {                                \
+        ip += step;                     \
+        if (ip > end) {                 \
+            goto l;                     \
+        }                               \
+    } while (0)
+
+#define WRITE_ADVANCE_CHECK(src, end, ip, data, l) \
+    do {                                           \
+        memcpy(&data, src + ip, sizeof(data));     \
+        ADVANCE_CHECK(end, ip, sizeof(data), l);   \
+    } while (0)
+
+DisassemblyError preprocessArg(CommandArgType argType, char const* inputBuffer, size_t numBytes, size_t* ipp, LabelTable* labelTable) {
+    assert(inputBuffer);
+    assert(ipp);
+    assert(labelTable);
+
+    size_t ip = *ipp;
+
+    switch (argType) {
+        case CMD_ARG_TYPE_INT:
+            ADVANCE_CHECK(numBytes, ip, sizeof(CPUInt), corruption);
+            break;
+        case CMD_ARG_TYPE_UINT:
+            ADVANCE_CHECK(numBytes, ip, sizeof(CPUUInt), corruption);
+            break;
+        case CMD_ARG_TYPE_FLOAT:
+            ADVANCE_CHECK(numBytes, ip, sizeof(CPUFloat), corruption);
+            break;
+        case CMD_ARG_TYPE_REGISTER: {
+            CPURegisterID cmdArg;
+            WRITE_ADVANCE_CHECK(inputBuffer, numBytes, ip, cmdArg, corruption);
+            Register const* reg = getRegisterByCode(cmdArg);
+            if (!reg) {
+                return DISASSEMBLY_INVALID_REGISTER_ID;
+            }
+        } break;
+        case CMD_ARG_TYPE_LABEL: {
+            size_t cmdArg = numBytes;
+            WRITE_ADVANCE_CHECK(inputBuffer, numBytes, ip, cmdArg, corruption);
+            if (cmdArg > numBytes) {
+                return DISASSEMBLY_INVALID_JUMP_ADDRESS;
+            }
+            if (addLabelUseByAddr(labelTable, cmdArg) != LABEL_ADD_OK) {
+                return DISASSEMBLY_OOM;
+            }
+        } break;
+        default:
+            goto corruption;
+    }
+
+    *ipp = ip;
+
+    return DISASSEMBLY_OK;
+
+corruption:
+    return DISASSEMBLY_CORRUPTION;
+}
+
+DisassemblyError preprocessCommand(char const* inputBuffer, size_t numBytes, size_t* ipp, LabelTable* labelTable) {
+    assert(inputBuffer);
+    assert(ipp);
+    assert(labelTable);
+
+    size_t ip = *ipp;
+
+    CPUCommandID cmdCode = CMD_INVALID_CODE;
+    WRITE_ADVANCE_CHECK(inputBuffer, numBytes, ip, cmdCode, corruption);
+
+    Command const* const cmd = getCommandByCode(cmdCode);
+    if (!cmd) {
+        return DISASSEMBLY_INVALID_COMMAND;
+    }
+
+    for (size_t i = 0; i < cmd->numArgs; i++) {
+        DisassemblyError res = preprocessArg(cmd->argType[i], inputBuffer, numBytes, &ip, labelTable);
+        if (res != DISASSEMBLY_OK) {
+            return res;
+        }
+    }
+
+    *ipp = ip;
+
+    return DISASSEMBLY_OK;
+
+corruption:
+    return DISASSEMBLY_CORRUPTION;
+}
+
+#undef ADVANCE_CHECK
+#undef WRITE_ADVANCE_CHECK
+
+DisassemblyError preprocessInput(char const* inputBuffer, size_t numBytes, LabelTable* labelTable) {
+    assert(inputBuffer);
+    assert(labelTable);
+
+    size_t ip = 0;
+    while (ip < numBytes) {
+        DisassemblyError res = preprocessCommand(inputBuffer, numBytes, &ip, labelTable);
+        if (res != DISASSEMBLY_OK) {
+            return res;
+        }
+    }
+    assert(ip == numBytes);
+
+    return DISASSEMBLY_OK;
+}
+
+#define ADVANCE(end, ip, step) \
+    do {                       \
+        ip += step;            \
+        assert(ip <= end);     \
+    } while (0)
+
+#define WRITE_ADVANCE(src, end, ip, data)      \
+    do {                                       \
+        memcpy(&data, src + ip, sizeof(data)); \
+        ADVANCE(end, ip, sizeof(data));        \
+    } while (0)
+
+DisassemblyError processArg(CommandArgType argType, char const* inputBuffer, size_t numBytes, size_t* ipp, LabelTable const* labelTable, FILE* outputFile) {
+    assert(inputBuffer);
+    assert(ipp);
+    assert(labelTable);
+    assert(outputFile);
+
+    size_t ip = *ipp;
+
+    fputc(' ', outputFile);
+    switch (argType) {
+        case CMD_ARG_TYPE_INT: {
+            CPUInt cmdArg;
+            WRITE_ADVANCE(inputBuffer, numBytes, ip, cmdArg);
+            fprintf(outputFile, "%" CPU_PFMT_I, cmdArg);
+        } break;
+        case CMD_ARG_TYPE_UINT: {
+            CPUUInt cmdArg;
+            WRITE_ADVANCE(inputBuffer, numBytes, ip, cmdArg);
+            fprintf(outputFile, "%" CPU_PFMT_UI, cmdArg);
+        } break;
+        case CMD_ARG_TYPE_FLOAT: {
+            CPUFloat cmdArg;
+            WRITE_ADVANCE(inputBuffer, numBytes, ip, cmdArg);
+            fprintf(outputFile, "%" CPU_PFMT_F, cmdArg);
+        } break;
+        case CMD_ARG_TYPE_REGISTER: {
+            CPURegisterID cmdArg;
+            WRITE_ADVANCE(inputBuffer, numBytes, ip, cmdArg);
+            Register const* reg = getRegisterByCode(cmdArg);
+            assert(reg);
+            fprintf(outputFile, "%s", reg->name);
+        } break;
+        case CMD_ARG_TYPE_LABEL: {
+            size_t cmdArg;
+            WRITE_ADVANCE(inputBuffer, numBytes, ip, cmdArg);
+            assert(cmdArg <= numBytes);
+            LabelTableEntry const* entry = findLabelByAddr(labelTable, cmdArg);
+            assert(entry);
+            printLabel(outputFile, entry - labelTable->data);
+        } break;
+        default:
+            assert(!"Cannot get here");
+            return DISASSEMBLY_CORRUPTION;
+    }
+
+    *ipp = ip;
+
+    return DISASSEMBLY_OK;
+}
+
+DisassemblyError processCommand(char const* inputBuffer, size_t numBytes, size_t* ipp, LabelTable const* labelTable, FILE* outputFile) {
+    assert(inputBuffer);
+    assert(ipp);
+    assert(labelTable);
+    assert(outputFile);
+
+    size_t ip = *ipp;
+
+    CPUCommandID cmdCode = CMD_INVALID_CODE;
+    WRITE_ADVANCE(inputBuffer, numBytes, ip, cmdCode);
+    Command const* const cmd = getCommandByCode(cmdCode);
+    assert(cmd);
+
+    fprintf(outputFile, COMMAND_INDENT "%s", cmd->name);
+
+    assert(cmd->numArgs <= COMMAND_MAX_ARGS);
+    for (size_t i = 0; i < cmd->numArgs; i++) {
+        DisassemblyError res = processArg(cmd->argType[i], inputBuffer, numBytes, &ip, labelTable, outputFile);
+        if (res != DISASSEMBLY_OK) {
+            return res;
+        }
+    }
+    fprintf(outputFile, "\n");
+
+    *ipp = ip;
+
+    return DISASSEMBLY_OK;
+}
+
+#undef ADVANCE
+#undef WRITE_ADVANCE
+
+LabelTableEntry const* processNextLabel(LabelTableEntry const* nextLabel, LabelTableEntry const* firstLabel, size_t ip, FILE* outputFile) {
+    assert(nextLabel);
+    assert(firstLabel);
+    assert(nextLabel >= firstLabel);
+    assert(outputFile);
+
+    if (ip == nextLabel->labelAddress) {
+        printLabel(outputFile, nextLabel - firstLabel);
+        fprintf(outputFile, "%c\n", LABEL_END);
+        nextLabel++;
+    }
+    return nextLabel;
+}
+
+DisassemblyError processInput(char const* inputBuffer, size_t numBytes, LabelTable const* labelTable, FILE* outputFile) {
+    assert(inputBuffer);
+    assert(labelTable);
+    assert(outputFile);
+
+    LabelTableEntry const* firstLabel = labelTable->data;
+    LabelTableEntry const* nextLabel = labelTable->data;
+
+    size_t ip = 0;
+    while (ip < numBytes) {
+        nextLabel = processNextLabel(nextLabel, firstLabel, ip, outputFile);
+        processCommand(inputBuffer, numBytes, &ip, labelTable, outputFile);
+    }
+    assert(ip == numBytes);
+    nextLabel = processNextLabel(nextLabel, firstLabel, ip, outputFile);
+    assert(nextLabel + 1 == firstLabel + labelTable->size);
+
+    return DISASSEMBLY_OK;
+}
+
 DisassemblyError disassemble(char const* const inputBuffer, const size_t numBytes, FILE* const outputFile) {
     assert(inputBuffer);
     assert(outputFile);
 
-    size_t bufI = 0;
-    while (bufI < numBytes) {
-        size_t cmdCode = CMD_INVALID_CODE;
-        if (bufI + sizeof(cmdCode) > numBytes) {
-            return DISASSEMBLY_CORRUPTION;
-        }
-        memcpy(&cmdCode, inputBuffer + bufI, sizeof(cmdCode));
-        bufI += sizeof(cmdCode);
-
-        Command const* const cmd = getCommandByCode(cmdCode);
-        if (!cmd) {
-            return DISASSEMBLY_INVALID_COMMAND;
-        }
-
-        fprintf(outputFile, "%s", cmd->name);
-        for (size_t i = 0; i < cmd->numArgs; i++) {
-            double cmdArg;
-            if (bufI + sizeof(cmdArg) > numBytes) {
-                return DISASSEMBLY_CORRUPTION;
-            }
-            memcpy(&cmdArg, inputBuffer + bufI, sizeof(cmdArg));
-            bufI += sizeof(cmdArg);
-            fprintf(outputFile, " %g", cmdArg);
-        }
-        fprintf(outputFile, "\n");
+    LabelTable* labelTable = createLabelTable();
+    if (!labelTable) {
+        goto oom;
     }
 
-    return DISASSEMBLY_OK;
+    DisassemblyError res = preprocessInput(inputBuffer, numBytes, labelTable);
+    if (res != DISASSEMBLY_OK) {
+        freeLabelTable(labelTable);
+        return res;
+    }
+
+    LabelAddRes addRes = addLabelUseByAddr(labelTable, numBytes + 1);
+    if (addRes != LABEL_ADD_OK) {
+        assert(addRes == LABEL_ADD_OOM);
+        goto oom;
+    }
+
+    sortByAddress(labelTable);
+
+    res = processInput(inputBuffer, numBytes, labelTable, outputFile);
+
+    freeLabelTable(labelTable);
+
+    return res;
+
+oom:
+    freeLabelTable(labelTable);
+
+    return DISASSEMBLY_OOM;
 }
 
 int main(int argc, char const* argv[]) {
@@ -70,13 +315,16 @@ int main(int argc, char const* argv[]) {
         printf("Usage: %s input_file output_file\n", (argc) ? (argv[0]) : ("program_name"));
         return -1;
     }
+
     char const* const inputFileName = argv[1];
     char const* const outputFileName = argv[2];
+
     FILE* inputFile = fopen(inputFileName, "rb");
     if (!inputFile) {
         printf("Failed to open %s for reading\n", inputFileName);
         return -1;
     }
+
     FILE* outputFile = fopen(outputFileName, "wb");
     if (!outputFile) {
         printf("Failed to open %s for writing\n", outputFileName);
@@ -91,6 +339,7 @@ int main(int argc, char const* argv[]) {
     fclose(inputFile);
     fclose(outputFile);
     free(buffer);
+
     if (res != DISASSEMBLY_OK) {
         printf("DISASSEMBLY ERROR: %s\n", getDisassemblyErrorString(res));
         return -1;
